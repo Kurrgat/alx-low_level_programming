83ee7730dcc4e4145a21e45237f96ae921f28be7
Kurrgat <mrcykj@gmail.com>
2023-08-10 11:37:55 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar", stderr);
-	fputs("2015-10-19\n", stderr);
+	fputs("and that piece of art is useful\" - Dora Korpar, ", stdout);
+	fputs("2015-10-19\n", stdout);
 	return (1);
 }
