b7149bd4c672d89aab586b12fd06c30a154ea87e
Kurrgat <mrcykj@gmail.com>
2023-08-24 08:27:20 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,23 +2,23 @@
 
 /**
  * leet - encodes a string into 1337
- * @str: string to encode
+ * @s: string to encode
  * Return: encoded string
  */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	while (str[++index1])
+	for (i = 0; *(s + i); i++)
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (str[index1] == leet[index2] ||
-			 str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
 	}
-	return (str);
+	return (s);
 }
