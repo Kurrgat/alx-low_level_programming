c469d1cf33889bcc2534e5fe60db49912951f9da
Kurrgat <mrcykj@gmail.com>
2023-08-31 09:00:51 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,36 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function that returns the natural square root of a number
- * @n: the number for which to calculate the square root
- * Return: the square root of n
- * _sqrt: returns the square root of a number
+ * sqrt_recursive - Recursive function to calculate the square root.
+ * @n: The number for which to calculate the square root.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The square root if found, or -1 if not found.
  */
-
-int _sqrt_recursion(int n)
+int sqrt_recursive(int n, int guess)
 {
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt(0, n));
+	int square = guess * guess;
+
+	if (square == n)
+		return (guess);
+	else if (square > n)
+		return (-1);
+	else
+		return (sqrt_recursive(n, guess + 1));
 }
 
 /**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number for which to calculate the square root.
  *
- * Return: the square root of n
+ * Return: The square root if found, or -1 if not found.
  */
-
-int _sqrt(int n, int x)
+int _sqrt_recursion(int n)
 {
-	if (n > x / 2)
+	if (n < 0) /* Check for negative input */
 		return (-1);
-	else if (n * n == x)
+	else if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt(n + 1, x));
+	else
+		return (sqrt_recursive(n, 1));
 }
