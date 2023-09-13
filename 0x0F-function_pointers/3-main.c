c9e3b20671585cb7b553f8100f7799e1fb6adcec
Kurrgat <mrcykj@gmail.com>
2023-09-13 08:53:48 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,41 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
 
 /**
- * main - Entry point
- * @argc: The number of command line arguments
- * @argv: An array of command line argument strings
- * Return: 0 on success, or exit with a status code on error
- */
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
+*/
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*func_ptr)(int, int);
+int num1, num2;
+char *op;
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		return (98);  /*Return an error status code*/
-	}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	func_ptr = get_op_func(argv[2]);
+num1 = atoi(argv[1]);
+op = argv[2];
+num2 = atoi(argv[3]);
 
-	if (func_ptr == NULL)
-	{
-		printf("Error\n");
-		return (99);  /*Return an error status code*/
-	}
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
 
-	result = func_ptr(num1, num2);
-	printf("%d\n", result);
-
-	return (0);
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", get_op_func(op)(num1, num2));
+return (0);
 }
